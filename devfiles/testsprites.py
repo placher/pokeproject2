886c4258e9dbd6efa5b539d550ef17a93e7644b8
J. Patrick Lacher <jlacher1@nd.edu>
2017-04-20 10:38:02 -0400

    slight reorganization

--- devfiles/testsprites.py
+++ devfiles/testsprites.py
@@ -13,6 +13,8 @@ from pygame.locals import *
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
+scrpitsdir = parentdir+'/scripts'
+sys.path.insert(0,scrpitsdir)
 
 import spritesheet
 
