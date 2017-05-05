ecff86f65897d6dcb0737a87b0d465bb8c0fa2cc
John Ryan Adams <jadams11@fitz91.helios.nd.edu>
2017-05-05 13:24:37 -0400

    The One Who Invites People Over To Play a Classic 'Board Game' involving a circled mat and a spinner

---
+++ scripts/twisted_host.py
@@ -0,0 +1,36 @@
+from twisted.internet.protocol import Factory
+from twisted.internet.protocol import Protocol
+from twisted.internet import reactor
+import sys
+import pygame
+from pygame.locals import *
+from scripts import player
+
+class HostProtocol(Protocol):
+	def __init__(self, enemyplayer, enemysprites):
+		self.enemyplayer = enemyplay
+		self.enemysprites = enemysprites
+
+	def dataReceived(self, data):
+		dataArray = data.split(" ")
+		self.enemyplayer.updateNetwork(dataArray[0:6])
+		self.enemysprites[0].updateNetwork(dataArray[7:8])
+		self.enemysprites[1].updateNetwork(dataArray[9:10])
+		self.enemysprites[2].updateNetwork(dataArray[11:12])
+		self.enemysprites[3].updateNetwork(dataArray[13:14])
+
+	def connectionMade(self):
+		print("Connected to Player2 Or Something")
+
+	def connectionLost(self):
+		print("Connection to Player2 Is Lost")
+		reactor.stop()
+
+class HostFactory(Factory):
+	def __init__(self, enemyPlayer, enemySprites):
+		self.enemyPlayer = enemyPlayer
+		self.enemySprites = enemySprites
+		self.Connection = HostProtocol(self.enemyPlayer, self.enemySprites)
+
+	def buildProtocol(self):
+		return self.Connection
